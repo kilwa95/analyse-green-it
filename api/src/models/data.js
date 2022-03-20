d19830a470d74c5a8cd08dd4d9c867dbb2787a7f
Abdellah Azabar <a.azabar93@gmail.com>
2022-03-20 16:41:32 +0100

    Docker + api + front

--- api/src/models/data.js
+++ api/src/models/data.js
@@ -5,15 +5,26 @@ class Data extends Model {}
 Data.init(
   {
     common: { type: DataTypes.STRING, allowNull: true },
-    isee_common: { type: DataTypes.NUMBER, allowNull: true },
-    department: { type: DataTypes.STRING, allowNull: true },
-    isee_department: { type: DataTypes.NUMBER, allowNull: true },
-    intercom: { type: DataTypes.STRING, allowNull: true },
+    dep: { type: DataTypes.STRING, allowNull: true },
     region: { type: DataTypes.STRING, allowNull: true },
-    pop: { type: DataTypes.FLOAT, allowNull: true },
-    score_global_dep: { type: DataTypes.FLOAT, allowNull: true },
-    score_global_intercom: { type: DataTypes.FLOAT, allowNull: true },
-    score_global_region: { type: DataTypes.FLOAT, allowNull: true },
+    pop: { type: DataTypes.STRING, allowNull: true },
+    score_global_dep: { type: DataTypes.STRING, allowNull: true },
+    score_global_region: { type: DataTypes.STRING, allowNull: true },
+    acces_information_dep: { type: DataTypes.STRING, allowNull: true },
+    acces_information_region: { type: DataTypes.STRING, allowNull: true },
+    acces_interface_numirique_dep: { type: DataTypes.STRING, allowNull: true },
+    acces_interface_numirique_region: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    competence_admin_dep: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    competence_admin_region: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
   },
   {
     sequelize: connection,
--- api/src/routes.js
+++ api/src/routes.js
@@ -7,17 +7,35 @@ const router = express.Router()
 router.use(express.json())
 
 // Swagger UI
-router.use('/', (req, res, next) => {
+router.get('/', (req, res, next) => {
   const results = []
   const file = path.join(__dirname, './content/data.csv')
   fs.createReadStream(file, { encoding: 'utf-16le' })
     .pipe(csv({ separator: '\t' }))
     .on('data', (data) => results.push(data))
     .on('end', () => {
-      const jsonData = JSON.stringify(results)
-      var jsonParsed = JSON.parse(jsonData)
-      // const resultFinal = results.map((item) => item['Libcom'])
-      console.log(jsonParsed)
+      const resultFinal = results.map((item) => {
+        return {
+          common: item['Libcom'],
+          dep: item['Libdep'],
+          region: item['Libreg'],
+          pop: item['P16 Pop'],
+          score_global_dep: item['SCORE GLOBAL departement'],
+          score_global_region: item['SCORE GLOBAL region'],
+          acces_information_dep: item["ACCES A L'INFORMATION departement"],
+          acces_information_region: item["ACCES A L'INFORMATION region"],
+          acces_interface_numirique_dep:
+            item['ACCÈS AUX INTERFACES NUMERIQUES departement'],
+          acces_interface_numirique_region:
+            item['ACCÈS AUX INTERFACES NUMERIQUES region'],
+          competence_admin_dep: item['COMPETENCES ADMINISTATIVES departement'],
+          competence_admin_region: item['COMPETENCES ADMINISTATIVES region'],
+        }
+      })
+      res.json(resultFinal)
+      // res.json(results)
+      // console.log(results[0]
+      console.log(resultFinal)
     })
 })
 
